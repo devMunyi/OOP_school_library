68db7247e468a0093584278fecaf7690e3de8e7f
devMunyi <samunyi90@gmail.com>
2022-12-20 14:44:14 +0300

    add required changes

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,6 @@ require './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @nameable_object.correct_name[0..9] if @nameable_object.correct_name.length > 10
+    @nameable_object.correct_name.length > 10 ? @nameable_object.correct_name[0..9] : @nameable_object.correct_name
   end
 end
