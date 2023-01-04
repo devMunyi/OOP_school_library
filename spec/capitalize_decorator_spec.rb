686ed24b722238214561c97c37c239dbc133f232
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2023-01-04 17:40:43 +0100

    Test trimmer_decorator & person methods

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../capitalize_decorator'
 
 describe CapitalizeDecorator do
-  let(:capitalize_decorator_obj) {double('testing capitalize', correct_name: 'sam')}
+  let(:capitalize_decorator_obj) { double('testing capitalize', correct_name: 'sam') }
 
   context 'Testing the capitalize decorator class' do
     it 'Shoud capitalize the first character' do
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,52 +1,52 @@
 require_relative '../teacher'
 
-RSpec.describe Teacher do 
-  before :each do 
+RSpec.describe Teacher do
+  before :each do
     @new_teacher = Teacher.new(28, 'Plantae', 'Jane Doe')
   end
 
-  describe '#new' do 
-    it 'takes four parameters and returns a Teacher object' do 
+  describe '#new' do
+    it 'takes four parameters and returns a Teacher object' do
       expect(@new_teacher).to be_an_instance_of(Teacher)
     end
   end
 
   describe '#name' do
-    it 'returns the correct name' do 
+    it 'returns the correct name' do
       expect(@new_teacher.name).to eq('Jane Doe')
-    end 
+    end
 
-    it 'can set the name correctly' do 
+    it 'can set the name correctly' do
       @new_teacher.name = 'John'
       expect(@new_teacher.name).to eq('John')
-    end 
+    end
   end
 
   describe '#age' do
-    it 'returns the correct age' do 
+    it 'returns the correct age' do
       expect(@new_teacher.age).to eq(28)
-    end 
+    end
 
-    it 'can set the age correctly' do 
+    it 'can set the age correctly' do
       @new_teacher.age = 40
       expect(@new_teacher.age).to eq(40)
-    end 
+    end
   end
 
   describe '#specialization' do
-    it 'returns the correct specialization' do 
+    it 'returns the correct specialization' do
       expect(@new_teacher.specialization).to eq('Plantae')
-    end 
+    end
   end
 
   describe '#parent_permission' do
-    it 'return \'true\' as the default value for parent permission' do 
+    it 'return \'true\' as the default value for parent permission' do
       expect(@new_teacher.parent_permission).to eq('true')
     end
   end
 
-  describe '#can_use_services?' do 
-    it 'returns true' do 
+  describe '#can_use_services?' do
+    it 'returns true' do
       expect(@new_teacher.can_use_services?).to eq(true)
     end
   end
